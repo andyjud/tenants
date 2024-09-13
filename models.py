c743d88344a9957aaf33f1eaa5a6f786e1269c6f
Andreas Jud <andreas.jud@gmail.com>
2024-09-13 14:48:43 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from colorfield.fields import ColorField 
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
+class SiteSetting(models.Model):
+    name = models.CharField(max_length=100)
+    color = ColorField(default='#000000') 
+    logo = models.ImageField(upload_to='logo/', null=True, blank=True)
+    
     def __str__(self):
         return self.name
