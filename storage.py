70eecabdf846e4bdb37a8bfdd4f0adc755056e4d
Andreas Jud <andreas.jud@gmail.com>
2024-09-27 18:47:05 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,14 +1,22 @@
 from django.core.files.storage import FileSystemStorage
 from django.db import connection
 from django_tenants.files.storage import TenantFileSystemStorage
+from django.conf import settings
+from storages.backends.s3 import S3Storage
 
 class CustomSchemaStorage:
     def _get_storage_backend(self):
         schema_name = connection.schema_name
-        if schema_name == 'public':
-            return FileSystemStorage()
+        if settings.ENVIRONMENT == 'development':
+            if schema_name == 'public':
+                return FileSystemStorage()
+            else:
+                return TenantFileSystemStorage()
         else:
-            return TenantFileSystemStorage()
+            if schema_name == 'public':
+                return S3Storage() 
+            else:
+                return S3TenantStorage(schema_name)
 
     def save(self, name, content, max_length=None):
         storage_backend = self._get_storage_backend()
@@ -25,3 +33,13 @@ class CustomSchemaStorage:
     def delete(self, name):
         storage_backend = self._get_storage_backend()
         storage_backend.delete(name)
+        
+        
+class S3TenantStorage(S3Storage):
+    def __init__(self, schema_name):
+        super().__init__()
+        self.schema_name = schema_name
+        
+    def save(self, name, content, max_length=None):
+        path = f"{settings.MULTITENANT_RELATIVE_MEDIA_ROOT % self.schema_name}/{name}"
+        return super().save(path, content, max_length)
