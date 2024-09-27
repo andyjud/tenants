ffd68063f231f8b9a1306787d0a155c87ddab501
Andreas Jud <andreas.jud@gmail.com>
2024-09-27 18:47:31 +0200

    Update models.py

--- tenants_manager/models.py
+++ tenants_manager/models.py
@@ -4,6 +4,8 @@ from django_tenants.models import TenantMixin, DomainMixin
 class Tenant(TenantMixin):
     name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
+    auto_create_schema = True
+    auto_drop_schema = True
     
 class Domain(DomainMixin):
     pass
