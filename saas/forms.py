e86c0b2ce894666f89fabf75440d78da3c88ed3f
Andreas Jud <andreas.jud@gmail.com>
2024-10-02 18:05:50 +0200

    Create forms.py

---
+++ saas/forms.py
@@ -0,0 +1,30 @@
+from django.forms import ModelForm
+from django import forms
+from django.core.exceptions import ValidationError
+import re
+from a_tenant_manager.models import *
+
+
+class TenantForm(ModelForm):
+    class Meta:
+        model = Tenant
+        fields = ['name', 'schema_name']
+        labels = {
+            'name' : 'Name',
+            'schema_name' : 'Schema Name',
+        }
+        widgets = {
+            'name' : forms.TextInput(attrs={'placeholder': 'Add Name ...'}),
+            'schema_name' : forms.TextInput(attrs={'placeholder': 'Add Subdomain ...'}),
+        } 
+        
+    def clean_schema_name(self):
+        schema_name = self.cleaned_data['schema_name'].lower() 
+
+        if not re.match(r'^[a-z0-9]+(?:-[a-z0-9]+)*$', schema_name):
+            raise ValidationError("Subdomains can only contain lowercase letters, numbers, and hyphens!")
+        
+        if Tenant.objects.filter(schema_name=schema_name).exists():
+            raise ValidationError("This subdomain is already taken.")
+        
+        return schema_name
